d808ca86ac535d6f9c5e7fe646a67ca790825183
Dandya <Aleksandrmen6@gmail.com>
2021-03-24 10:56:15 +0300

    add mod

---
+++ Python/moduls.py
@@ -0,0 +1,9 @@
+def modul(coef = [0], mod = 10):
+    if type(coef) is not list:
+        return coef%mod
+    newCoef = []
+    for digit in a:
+        newCoef.append(digit%mod)
+    return newCoef
+
+def comparison()
