f8b1a0aa18c9115a771c77897f33bc1c5852e110
Dandya <70721037+Dandya@users.noreply.github.com>
2021-08-04 12:24:27 +0500

    Ok

--- Python/randomZ.py
+++ Python/randomZ.py
@@ -6,4 +6,26 @@ def moreRandomAB(A,B):
     random.seed(version=2)
     for iteration in range(countDigits):
         arrRandomNumbers.append(random.randint(A,B))
+    arrRandomNumbers = sortArr(arrRandomNumbers, countDigits)
     return arrRandomNumbers
+
+def sortArr(arr, sizeArr)->list:
+    checkEnd = False
+    indexElm = 0
+    checkP = False
+    while not checkEnd:
+        if arr[indexElm] > arr[indexElm+1]:
+            buf = arr[indexElm]
+            arr[indexElm] = arr[indexElm+1]
+            arr[indexElm+1] = buf
+            checkP = True
+        indexElm += 1
+        if indexElm == sizeArr-1:
+            if checkP == True:
+                indexElm = 0
+                checkP = False
+            else:
+                checkEnd = True
+    return arr
+
+
